894fe46cad4928b7fa9f5fe65c29cb4e3dfc1abe
AbhinavGiri21 <abhinavgiri55@gmail.com>
2025-07-08 11:34:00 +0530

    .env updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const isProduction = process.env.NODE_ENV === "production";
+
+export const API_BASE_URL = isProduction
+    ? process.env.REACT_APP_API_BASE_URL
+    : "http://localhost:5000";            
